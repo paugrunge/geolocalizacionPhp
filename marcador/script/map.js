6635e0301be14124d4a4accf5118f9917cd277f8
Pau <paulaalbora@hotmail.com>
2015-08-24 23:29:18 -0300

    Division de javascript en modulos bajo un namespace

--- marcador/script/map.js
+++ marcador/script/map.js
@@ -1,110 +1,123 @@
-function mostrarGoogleMaps() {
-    //Creamos el punto a partir de las coordenadas:
-    var punto = new google.maps.LatLng(-34.603657,-58.381794);
+// esta variable hara de namespace
+var geolocalizacion = geolocalizacion || {};
 
-    //Configuramos las opciones indicando Zoom, punto(el que hemos creado) y tipo de mapa
-    var myOptions = {
-    zoom: 14, center: punto, mapTypeId: google.maps.MapTypeId.ROADMAP //G_MAP_TYPE o ROADMAP
-    };
+//Lo asignado a self sera es publico, es decir se puede acceder utilizando geolocalizacion.
+//lo definido solo como var sera privado
 
-
-    map = new google.maps.Map(document.getElementById("mostrarMapa"), myOptions);
-
-    bounds = new google.maps.LatLngBounds();
+//Envolvemos el módulo dentro de una función auto-ejecutable
+(function (self){
     
-    countMarker = 0;
-    
-    puntos = [];
+    var mostrarGoogleMaps = function mostrarGoogleMaps() {
+        //Creamos el punto a partir de las coordenadas:
+        var punto = new google.maps.LatLng(-34.603657,-58.381794);
 
-  };
-
-function verMarcador(){
-    
-    countMarker++;
-    $("#divError").hide();
-    
-    var lat = $("#lat").val();
-    var lng = $("#lng").val();
-    var nombre = $("#nombre").val();
-   
-    
-    if(validar(lat,lng)){
-
-        var punto = new google.maps.LatLng(lat,lng);
-        
-        var marker=new google.maps.Marker({
-          position:punto,
-          map : map,
-          title: nombre
-          });
-
-        // la variable bounds utiza el marcador
-         bounds.extend(punto); 
-
-        //Adecuo los limites del mapa de acuerdo a variable bounds
-          map.fitBounds(bounds);
-
-        //Para volver a acomodar el zoom luego de adecuar los
-        // limites del mapa al marcador
-        if(countMarker == 1)
-        {
-            var listener = google.maps.event.addListener(map, "idle",               function () {
-                map.setZoom(14);
-               google.maps.event.removeListener(listener);
-            });
-        }
-        
-        //agrego al array de marcadores por si elijen guardar
-        var p = {
-            "lat" : lat,
-            "lng": lng,
-            "nombre": nombre 
-        }
-
-        puntos.push(p);
-
-        console.log(puntos);
-
-    }
-    else{
-        $("#divError").html("Debe ingresar latitud y longitud numericas validas");
-         $("#divError").show();
-                            
-    }
-    
-    
-}
-
-function validar(lat,lng){
-    console.log(isNaN(lat));
-    console.log(isNaN(lng));
-    if(lat == "" || lng == "")
-    {
-        console.log("retorno falso");
-        return false;
-    }
-    else 
-    {
-        console.log("retorno true");
-        return true;
-    }
-}
-
-function guardar(){
-    var parametros = {
-                "valorCaja1" : "fjfj"
+        //Configuramos las opciones indicando Zoom, punto(el que hemos creado) y tipo de mapa
+        var myOptions = {
+        zoom: 14, center: punto, mapTypeId: google.maps.MapTypeId.ROADMAP //G_MAP_TYPE o ROADMAP
         };
+
+
+        map = new google.maps.Map(document.getElementById("mostrarMapa"), myOptions);
+
+        bounds = new google.maps.LatLngBounds();
+
+        countMarker = 0;
+
+        puntos = [];
+
+      };
     
-    var promise = $.ajax
-	                ({
-	                type: "POST",
-	                url: "nexoAjax.php",
-	                data: ({"marcadores" : puntos}),
-	                cache: false,
-	                dataType: "text"
-	              });// fin del ajax
+     google.maps.event.addDomListener(window, 'load', mostrarGoogleMaps);
+
+
+    self.verMarcador = function verMarcador(){
+
+        countMarker++;
+        $("#divError").hide();
+
+        var lat = $("#lat").val();
+        var lng = $("#lng").val();
+        var nombre = $("#nombre").val();
+
+        if(validar(lat,lng)){
+            
+        
+//             Ahora que tengo el modulo utilizo la funcion que ya defini en el otro script para crear puntos
+//            var punto = new google.maps.LatLng(lat,lng);
+//
+//            var marker=new google.maps.Marker({
+//              position:punto,
+//              map : map,
+//              title: nombre
+//              });
+//
+//            // la variable bounds utiza el marcador
+//             bounds.extend(punto); 
+//
+//            //Adecuo los limites del mapa de acuerdo a variable bounds
+//              map.fitBounds(bounds);
+//
+//            //Para volver a acomodar el zoom luego de adecuar los
+//            // limites del mapa al marcador
+//            if(countMarker == 1)
+//            {
+//                var listener = google.maps.event.addListener(map, "idle",function () {
+//                    map.setZoom(14);
+//                   google.maps.event.removeListener(listener);
+//                });
+//            }
+
+            //agrego al array de marcadores por si elijen guardar
+            var p = {
+                "lat" : lat,
+                "lng": lng,
+                "nombre": nombre,
+                "direccion" : "",
+                "codPostal" : ""
+            }
+
+            puntos.push(p);
+
+             // Nueva infowindow, el tooltip que aparece al hacer click
+            var infoWindow = new google.maps.InfoWindow();
+            
+            geolocalizacion.displayMarkers(puntos, infoWindow);
+            console.log(puntos);
+
+        }
+        else{
+            $("#divError").html("Debe ingresar latitud y longitud numericas validas");
+             $("#divError").show();
+
+        }
+
+
+    }
+
+    var validar = function validar(lat,lng){
+
+        if(lat == "" || lng == "")
+            return false;
+        else 
+            return true;
+
+    }
+
+    self.guardar = function guardar(){
+
+
+        var promise = $.ajax
+                        ({
+                        type: "POST",
+                        url: "nexoAjax.php",
+                        data: ({"marcadores" : puntos}),
+                        cache: false,
+                        dataType: "text"
+                      });// fin del ajax
+
+        promise.done(function (dato){
+            alert(dato);
+        });
+    }
     
-    promise.done(function (dato){
-        alert(dato);
-    });
-}
+})(geolocalizacion);
--- multiplesMarcadores/map.js
+++ multiplesMarcadores/map.js
@@ -1,109 +1,125 @@
-var marcadores = [
-    {
-        name:"León", 
-         lat: 42.603,
-         lng: -5.577,
-         address: "Algo 534543",
-         postalCode: "1940"
-    },
-    {
-        name:"Salamanca", 
-         lat: 40.963,
-         lng: -5.669,
-         address: "Some 534543",
-         postalCode: "1942"
-    },
-    {
-        name:"Zamora", 
-        lat: 41.503,
-        lng: -5.744,
-        address: "Calle 534543",
-        postalCode: "1944"
-    },
-    {
-        name:"Algo", 
-        lat: 42.000,
-        lng: -6.000,
-        address: "Low 534543",
-        postalCode: "1945"
+//Este patrón de diseño JavaScript, que se basa en el expuesto en la publicación de Addy Osmani Namespacing Patterns, 
+//declara una variable geolocalizacion que será la que contendrá todo el espacio de variables de la página. 
+//Si esa variable ya fue declarada anteriormente ahora la vuelve a referenciar. En otro caso le asigna un objeto vacío
+
+//Asignación inicial a un objeto literal vacío o a si mismo
+//si ya fue creado previamente.
+var geolocalizacion = geolocalizacion || {};
+
+//Lo asignado a self sera es publico, es decir se puede acceder utilizando geolocalizacion.
+//lo definido solo como var sera privado
+//Envolvemos el módulo dentro de una función auto-ejecutable
+(function (self){
+    var marcadores = [
+        {
+            nombre:"León", 
+             lat: 42.603,
+             lng: -5.577,
+             direccion: "Algo 534543",
+             codPostal: "1940"
+        },
+        {
+            nombre:"Salamanca", 
+             lat: 40.963,
+             lng: -5.669,
+             direccion: "Some 534543",
+             codPostal: "1942"
+        },
+        {
+            nombre:"Zamora", 
+            lat: 41.503,
+            lng: -5.744,
+            direccion: "Calle 534543",
+            codPostal: "1944"
+        },
+        {
+            nombre:"Algo", 
+            lat: 42.000,
+            lng: -6.000,
+            direccion: "Low 534543",
+            codPostal: "1945"
+        }
+    ];
+    
+    
+    var initialize = function initialize() {
+
+
+       var mapOptions = {
+          center: new google.maps.LatLng(41.503, -5.744),
+          zoom: 7,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+       };
+
+       map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+
+       // Nueva infowindow, el tooltip que aparece al hacer click
+       infoWindow = new google.maps.InfoWindow();
+
+       // Evento que cierra infowindow con un click en el mapa
+       google.maps.event.addListener(map, 'click', function() {
+          infoWindow.close();
+       });
+
+       // Se llama para comenzar con la creacion de los marcadores
+       self.displayMarkers(marcadores, infoWindow);
     }
-];
-
-function initialize() {
     
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+    self.displayMarkers =  function displayMarkers(marcadores, infoWindow){
+
+       // Esta variable setea los limites y el zoom de acuerdo
+    // a la posicion de los marcadores
+       var bounds = new google.maps.LatLngBounds();
+
+       // For loop that runs through the info on markersData making it possible to createMarker function to create the markers
+       for (var i = 0; i < marcadores.length; i++){
+
+          var latlng = new google.maps.LatLng(marcadores[i].lat, marcadores[i].lng);
+          var nombre = marcadores[i].nombre;
+          var dire = marcadores[i].direccion;
+          var codPostal = marcadores[i].codPostal;
+
+          //La creacion del marcador
+          createMarker(latlng, nombre, dire, codPostal, infoWindow);
+
+          // Cada lat y lng de los marcadores se agrefa a bounds
+          bounds.extend(latlng); 
+       }
+
+       // La variables es usada para setear los limites
+       // con API’s fitBounds() function
+       map.fitBounds(bounds);
+    }
+
+    // Crea un marcador con los parametros que recibe, funcion privada que se llama desde la funcion publica displayMarkers
+    var createMarker =  function createMarker(latlng, nombre, direccion, codPostal, infowin){
+
+        var marker = new google.maps.Marker({
+          map: map,
+          position: latlng,
+          title: nombre
+       });
+
+       // Evento click en marcador
+       // Cuando se dispara el evento se crea infowindow
+       // y se abre
+       google.maps.event.addListener(marker, 'click', function() {
+
+          // Variable para definir el html del tooltip infowindow
+          var iwContent = '<div id="iw_container">' +
+          '<div class="iw_title">' + nombre + '</div>' +
+          '<div class="iw_content">' + direccion + '<br />' +
+          codPostal + '</div></div>';
+
+          // Seteandole el contenido
+          infowin.setContent(iwContent);
+
+          // Abrriendo
+          infowin.open(map, marker);
+       });
+    }
     
-   var mapOptions = {
-      center: new google.maps.LatLng(41.503, -5.744),
-      zoom: 7,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-   };
-
-   map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
-
-   // Nueva infowindow, el tooltip que aparece al hacer click
-   infoWindow = new google.maps.InfoWindow();
-
-   // Evento que cierra infowindow con un click en el mapa
-   google.maps.event.addListener(map, 'click', function() {
-      infoWindow.close();
-   });
-
-   // Se llama para comenzar con la creacion de los marcadores
-   displayMarkers();
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
-        
-    function displayMarkers(){
-
-   // Esta variable setea los limites y el zoom de acuerdo
-// a la posicion de los marcadores
-   var bounds = new google.maps.LatLngBounds();
-
-   // For loop that runs through the info on markersData making it possible to createMarker function to create the markers
-   for (var i = 0; i < marcadores.length; i++){
-
-      var latlng = new google.maps.LatLng(marcadores[i].lat, marcadores[i].lng);
-      var name = marcadores[i].name;
-      var address = marcadores[i].address;
-      var postalCode = marcadores[i].postalCode;
-       
-      //La creacion del marcador
-      createMarker(latlng, name, address, postalCode);
-
-      // Cada lat y lng de los marcadores se agrefa a bounds
-      bounds.extend(latlng); 
-   }
-
-   // La variables es usada para setear los limites
-   // con API’s fitBounds() function
-   map.fitBounds(bounds);
-}
-
-// Crea un marcador con los parametros que recibe
-function createMarker(latlng, name, address, postalCode){
-   
-    var marker = new google.maps.Marker({
-      map: map,
-      position: latlng,
-      title: name
-   });
-
-   // Evento click en marcador
-   // Cuando se dispara el evento se crea infowindow
-   // y se abre
-   google.maps.event.addListener(marker, 'click', function() {
-      
-      // Variable para definir el html del tooltip infowindow
-      var iwContent = '<div id="iw_container">' +
-      '<div class="iw_title">' + name + '</div>' +
-      '<div class="iw_content">' + address + '<br />' +
-      postalCode + '</div></div>';
-      
-      // Seteandole el contenido
-      infoWindow.setContent(iwContent);
-
-      // Abrriendo
-      infoWindow.open(map, marker);
-   });
-}
+})(geolocalizacion);
