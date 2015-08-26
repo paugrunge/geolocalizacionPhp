88ad51802c492c9bc5db66106b0b20e544cfc7cc
Paula <paulaalbora@hotmail.com>
2015-08-26 15:56:12 +0000

    Lectura de archivo Excel con PHP-ExcelReader

---
+++ testLeerExcel.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'Excel/reader.php';
+
+$data = new Spreadsheet_Excel_Reader();
+$data->setOutputEncoding('CP1251');
+$data->read('fichero.xls');
+echo("<table>");
+for ($i = 1; $i <= $data->sheets[0]['numRows']; $i++) {
+	echo("<tr>");
+	for ($j = 1; $j <= $data->sheets[0]['numCols']; $j++) {
+		echo("<td>".$data->sheets[0]['cells'][$i][$j] ."</td>");
+	}
+	echo("</tr>");
+
+}
+echo("</table>");
+?>
