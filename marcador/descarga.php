3b098847a769a03708e0987061717f7f9f9b15f8
Paula <paulaalbora@hotmail.com>
2015-08-27 04:00:35 +0000

    Correccion en descarga

--- marcador/descarga.php
+++ marcador/descarga.php
@@ -1,16 +1,20 @@
 <?php
   session_start();
-  $file =  $_SESSION['file'];
-  if (file_exists ($file))
+  if (isset($_SESSION['file']))
   {
-        $basefichero = basename($file);
-        
-        header("Content-Type: application/octet-stream");
-        header("Content-Length: ".filesize($file));
-        header("Content-Disposition: attachment; filename=".$basefichero."");
-        readfile($file);
+    $file =  $_SESSION['file'];
+    if (file_exists ($file))
+    {
+          $basefichero = basename($file);
+          
+          header("Content-Type: application/octet-stream");
+          header("Content-Length: ".filesize($file));
+          header("Content-Disposition: attachment; filename=".$basefichero."");
+          readfile($file);
+    }
+    else
+      echo "No hay archivo para descargar, debe guardar primero.";
   }
   else
-    echo "No hay archivo para descargar, debe guardar primero.";
-   
+      echo "No hay archivo para descargar, debe guardar primero.";
 ?>
